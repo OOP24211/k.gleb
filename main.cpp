3bb1d865a7ba4fa74500769567ae8ac66d33c123
g.kozulin <g.kozulin@g.nsu.ru>
2025-09-25 15:14:00 +0700

    Перенёс Buffer в класс BufferManager

--- main.cpp
+++ main.cpp
@@ -6,23 +6,13 @@
 #include <windows.h>
 #include <iostream>
 #include <map>
-#define BUFFER_SIZE  (1024 * 1024) // 1 MB
+
 
 struct Word_stats {
     std::string word;
     int cnt = 0; // количество вхождений слова
     double frequency = 0.0;
 };
-
-struct Buffer {
-    std::ifstream* file_in;           // указатель на входной файл
-    std::map<std::string, Word_stats>* words_map;
-    size_t* total_words;                 // указатель на счётчик всех слов в тексте
-    std::string current_word;
-    char buffer[BUFFER_SIZE];         // буфер чтения
-           // текущее собираемое слово
-};
-
 
 namespace LetterUtils {
     static bool is_letter(unsigned char const symbol) {
@@ -47,66 +37,65 @@ namespace LetterUtils {
 
 
 
+class BufferManager {
+    static constexpr size_t BUFFER_SIZE = (1024*1024);
 
+    char buffer_[BUFFER_SIZE];
+    std::string current_word_;
 
+    std::ifstream& file_;
+    std::map<std::string, Word_stats>& words_map_;
+    size_t& total_words_;
+     void add_word(std::string& word) {
+        if (word.empty()) return;
 
-
-void init_buffer(Buffer& buff, std::ifstream& file, std::map<std::string, Word_stats>& words_map, size_t& total) {
-    buff.file_in = &file;
-    buff.words_map = &words_map;
-    buff.total_words = &total;
-    buff.current_word.clear();
-}
-
-void add_word(std::string& word, const Buffer& buff) {
-    if (word.empty() || !buff.words_map) return;
-
-    auto& map = *buff.words_map;
-    if (auto const is_found= map.find(word); is_found != map.end()) {
-        is_found->second.cnt++;
-    } else {
-        map[word].cnt++;
+        auto& map = words_map_;
+        if (auto const is_found= map.find(word); is_found != map.end()) {
+            is_found->second.cnt++;
+        } else {
+            map[word].cnt++;
+        }
+        if (total_words_) {
+            (total_words_)++;
+        }
+        word.clear();
     }
-    if (buff.total_words) {
-        (*buff.total_words)++;
-    }
-    word.clear();
-}
+     void process_block(std::streamsize const bytes_read_count) {
+        for (int j = 0; j < bytes_read_count; ++j) {
 
-
-void process_block(Buffer& buff, std::streamsize const bytes_read_count) {
-    std::string& current_word = buff.current_word;
-
-    for (int j = 0; j < bytes_read_count; ++j) {
-
-        if (auto symbol = static_cast<unsigned char>(buff.buffer[j]);LetterUtils::is_letter(symbol)){
-            LetterUtils::to_low_reg(symbol);
-            current_word += static_cast<char>(symbol);
+            if (auto symbol = static_cast<unsigned char>(buffer_[j]);LetterUtils::is_letter(symbol)){
+                LetterUtils::to_low_reg(symbol);
+                current_word_ += static_cast<char>(symbol);
             } else {
-                add_word(current_word, buff);
+                add_word(current_word_);
             }
+        }
     }
-}
+public:
+    BufferManager(std::ifstream& file,
+                  std::map<std::string, Word_stats>& words_map,
+                  size_t& total)
+    :  file_(file), words_map_(words_map), total_words_(total),buffer_{} {}
+    void process_file() {
+        while (file_.read(buffer_, BUFFER_SIZE)) {
+            std::streamsize const bytes_read_count = file_.gcount();
+            process_block(bytes_read_count);
+        }
 
-void process_file(Buffer& buff) {
-    std::ifstream& file_in = *buff.file_in;
+        // Обработать остаток (если файл закончился не на границе буфера)
+        if (std::streamsize const bytes_read_count = file_.gcount(); bytes_read_count > 0) {
+            process_block(bytes_read_count);
+        }
 
-    while (file_in.read(buff.buffer, BUFFER_SIZE)) {
-        std::streamsize const bytes_read_count = file_in.gcount();
-        process_block(buff, bytes_read_count);
+        // Обработать последнее слово, если осталось
+        add_word(current_word_);
+        current_word_.clear();
     }
 
-    // Обработать остаток (если файл закончился не на границе буфера)
-    if (std::streamsize const bytes_read_count = file_in.gcount(); bytes_read_count > 0) {
-        process_block(buff, bytes_read_count);
-    }
 
-    // Обработать последнее слово, если осталось
-    add_word(buff.current_word, buff);
-    buff.current_word.clear();
-}
+};
 
-void calculate_frequency(Buffer& buff, std::map<std::string, Word_stats>& Words_info) {
+void calculate_frequency(std::map<std::string, Word_stats>& Words_info) {
     for (auto& [word, Word_stats] : Words_info) {
         Word_stats.frequency = static_cast<double>(Word_stats.cnt) /  static_cast<double>(*buff.total_words);
     }
@@ -151,8 +140,8 @@ int main(int argc, char* argv[]) {
     std::map<std::string,Word_stats> Words_info;
     size_t total = 0;
     Buffer buff;
-    init_buffer(buff, file_input, Words_info, total);
-    process_file(buff);
+    BufferManager::init_buffer(buff, file_input, Words_info, total);
+    BufferManager::process_file(buff);
     file_input.close();
     calculate_frequency(buff,Words_info);
     sort_and_input_toCSV(file_output_name, Words_info);
